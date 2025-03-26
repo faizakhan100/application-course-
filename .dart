a08c3f5283e2954b203a48f79dcb34ae1a5b2a9d
faizakhan100 <wk702554@gmail.com>
2025-03-26 11:38:26 +0500

    course

---
+++ .dart
@@ -0,0 +1,114 @@
+void main()
+String task = '1';
+  switch (task){
+      case '1':
+       print (""
+  ;var math = 50;
+  var urdu = 60;
+  var sci = 30;
+  var islamiat = 43;
+  var english = 50;
+  var pakStudy =40;
+  var gk = 45;
+  
+
+ var obt = math + urdu + sci + islamiat +english + pakStudy +gk;
+  print("totalmarks =  $obt");
+ var per = obt / 700 * 100;
+   print("Percentage ${per.toStringAsFixed(2)} % ");
+   
+
+ if (per <= 100 && per >=80){print("Grade A1");}
+  
+   else
+      if (per <= 79 && per >= 70){print("Grade A");}
+
+   else
+      if (per <= 69 && per >= 60){print("Grade B");}
+   else
+      if (per <= 59 && per >= 50){print("Grade C");}
+   else
+      if (per <= 49){print("Fail");}
+  "");
+      
+     break;
+     
+      
+      
+    case '2':
+       print ("("
+       
+  var num1 = 42;
+  var num2 = 50;
+
+  var sum = num1 + num2;
+  var sub = num1 - num2;
+  var multi = num1 * num2;
+  var div = num1 / num2;
+
+  print("the total sum is $sum.");
+  print("the total substraction is $sub.");
+  print("the total multiplication is $multi.");
+  print("the total division is $div.");
+ ")");
+      
+      
+      
+      
+   
+      
+      case '3':
+       print ("int number =9;
+  
+  if (number %2 ==0){
+    print ("even");
+    
+  }
+  else {print ("odd");}");
+     
+      
+      
+      
+      
+      case '4':
+       print ("int year =2002;
+  if (year % 4== 2){print ("leap year");}
+  else {print ("its not a leap year");} ");
+      
+      
+      
+      case '5':
+       print (""
+  String vowels = 'z';
+  switch (vowels){
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+ print ("its a vowels");
+  
+  default:
+      {
+        print("its not a vowels");}
+      
+}
+  "");
+      
+      case '6':
+       print (""for(var i = 12-1; i >= 0; i--) {
+    print("list${i+1}");
+  }"");
+      
+  
+  
+  default:
+      {
+        print("not available further");
+      
+}
+  }  
+
+  
+  
+}
